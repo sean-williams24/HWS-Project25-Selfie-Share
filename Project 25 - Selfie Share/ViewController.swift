35cbd6f33840d86647d67f6d2008187d4a327a5c
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2019-11-04 16:26:44 +0000

    Initial Commit

---
+++ Project 25 - Selfie Share/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 25 - Selfie Share
+//
+//  Created by Sean Williams on 04/11/2019.
+//  Copyright © 2019 Sean Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
