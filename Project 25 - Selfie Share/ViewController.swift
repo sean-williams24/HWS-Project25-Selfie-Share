0df8a794b3b884a22b135af0a3f83451b07605e3
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2019-11-05 09:01:20 +0000

    FEAT: Alert controller for options of joining session or starting hosting session. MC session methods.

--- Project 25 - Selfie Share/ViewController.swift
+++ Project 25 - Selfie Share/ViewController.swift
@@ -6,21 +6,54 @@
 //  Copyright © 2019 Sean Williams. All rights reserved.
 //
 
+import MultipeerConnectivity
 import UIKit
 
-class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, MCSessionDelegate, MCBrowserViewControllerDelegate {
     
     var images = [UIImage]()
+    var peerID = MCPeerID(displayName: UIDevice.current.name)
+    var mcSession: MCSession?
+    var mcAdvertiserAssitant: MCAdvertiserAssistant?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Selfie Share"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showConnectionPrompt))
+        
+        mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
+        mcSession?.delegate = self
+        
     }
 
 
+    //MARK: - MCSession Methods
+    
+    func startHosting(action: UIAlertAction) {
+        guard let mcSession = mcSession else { return }
+        mcAdvertiserAssitant = MCAdvertiserAssistant(serviceType: "hws-project25", discoveryInfo: nil, session: mcSession)
+        mcAdvertiserAssitant?.start()
+    }
+    
+    func joinSession(action: UIAlertAction) {
+        guard let mcSession = mcSession else { return }
+        let mcBrowser = MCBrowserViewController(serviceType: "hws-project25", session: mcSession)
+        mcBrowser.delegate = self
+        present(mcBrowser, animated: true)
+    }
+    
 
+    //MARK: - Private Methods
+    
+    @objc func showConnectionPrompt() {
+        let ac = UIAlertController(title: "Connect to others", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Host a session", style: .default, handler: startHosting))
+        ac.addAction(UIAlertAction(title: "Join a session", style: .default, handler: joinSession))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
     
     
     //MARK: - Collection View Data Source
